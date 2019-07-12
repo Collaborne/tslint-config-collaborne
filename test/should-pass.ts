204403b5e727a646f82c52eef5cb96562b4e645e
Andreas Kohn <andreas.kohn@gmail.com>
2019-07-12 11:45:14 +0200

    Allow leading underscores in variable names
    This prevents a "fight" between tsc and tslint: tsc errors out when parameters are unused in functions, and
    allows to use a `_`-prefix to disable that error. But, then tslint will complain that the variable name isn't
    allowed.
    
    Let tsc win, and accept the underscores. This does lead to issues for other code where we still use the convention
    that an `_`-prefix  signifies private members: These are no longer going to be flagged as problems.
    
    See https://www.reddit.com/r/typescript/comments/9zp4co/parameters_not_used_in_function/ and other discussiones
    about the TS6133 error.

--- test/should-pass.ts
+++ test/should-pass.ts
@@ -6,3 +6,10 @@ const foo = 'foo';
 const object: MyInterface = {
 	foo: foo,
 };
+
+// A function with an unused first argument: `_` is used to tell the compiler that TS6133 "unused parameter"
+// is not relevant.
+function withUnusedParameter(_unusedParam: string, usedParam: string) {
+	// tslint:disable-next-line no-console
+	console.log(usedParam);
+}
